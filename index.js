2a45cf486fdaeb80ae6c5c5a71349d8093cb493e
Noah <noahmwangi@gmail.com>
2023-03-11 18:45:28 +0300

    Something is wrong with this test

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+newHeader.id = "victory";
+newHeader.textContent = "NOAH is the champion";
+
